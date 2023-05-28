dfabd12535ba3fcea61f995c64f744393b68ab86
MauricioBravo <qwe455r@gmail.com>
2023-05-27 21:48:04 -0300

    Create Strings using Template Literals finished

--- ES6/test.js
+++ ES6/test.js
@@ -26,4 +26,32 @@ const person = {
   
   console.log(greeting);
 
+  
+/*      Create Strings using Template Literals        */
 
+  const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+  };
+  function makeList(arr) {
+    // Only change code below this line
+    //const failureItems = [i] =;
+    const failureItems=[];
+      for(let i = 0; i<3;i++){
+      failureItems [i] = `<li class="text-warning">${result.failure[i]}</li>`;
+      }
+    // Only change code above this line
+  
+    return failureItems;
+  }
+  
+  const failuresList = makeList(result.failure);
+  console.log(failuresList);
+/*
+  [
+    '<li class="text-warning">no-var</li>',
+    '<li class="text-warning">var-on-top</li>',
+    '<li class="text-warning">linebreak</li>'
+  ]
+  */

cc8fc0b5a0666fe8a7fcaf619510e8d804f22a2d
MauricioBravo <qwe455r@gmail.com>
2023-07-03 11:33:02 -0300

    basic data structures 2

--- Basic Data Structures/basicDataStructures test.js
+++ Basic Data Structures/basicDataStructures test.js
@@ -17,13 +17,30 @@ function forecast(arr) {
   let newArr = [];
   while (num >= 1) {
     // Only change code below this line
-    newArr=[...arr];
+    newArr.push([...arr]);
     console.log(newArr);
-    num--;
     // Only change code above this line
     num--;
   }
   return newArr;
 }
 
-console.log(copyMachine([true, false, true], 2));
+console.log(copyMachine([true, false, true], 2));blob:file:///fc2589cb-04e7-4489-b3d9-67f85653c6f6
+
+
+
+// INDEXOF PARA BUSCAR EN ARREGLO COINCIDENCIAS
+
+function quickCheck(arr, elem) {
+    // Only change code below this line
+    console.log(arr.indexOf(elem));
+    if(arr.indexOf(elem)>=0){
+        return true;
+    }
+    else return false;
+    // Only change code above this line
+  }
+  
+  console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+  console.log(quickCheck(["squash", "onions", "shallots"], "mushrooms"));
+  console.log(quickCheck(["onions", "squash", "shallots"], "onions"));

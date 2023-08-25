4740af0f381dfc764a0f36fd869f2d8d303c8dbf
MauricioBravo <qwe455r@gmail.com>
2023-08-25 14:42:28 -0300

    confirm the ending finished

--- Basic Algorithm Scripting/confirm the ending.js
+++ Basic Algorithm Scripting/confirm the ending.js
@@ -1,21 +1,34 @@
 
 
 function confirmEnding(str, target) {
-  
-    let targetLenth=target.length;
-    console.log(targetLenth);
-// quizá crear una funcion que revise letra por letra la comparación del final del str con el de target
-    let length=str.length-1;
-    console.log(length);
 
-    if(str[length]==target){
-      return true;
-    }
-        else{
-            return false;
+    let targetLength = target.length;
+    console.log(targetLength);
+    let strLength = str.length;
+    console.log(strLength);
+
+    let posicion = strLength - targetLength;
+    console.log(posicion);
+
+    let i = posicion;
+    let j = 0;
+    var flag = true;
+
+    for (let x = 0; x < targetLength; x++) {
+        console.log(i);
+
+        console.log(str[i]);
+        console.log(target[i]);
+
+        if (str[i] != target[j]) {
+            flag = false;
         }
+        console.log(flag);
+        i++;
+        j++;
+    }
+return flag;
+}
 
-  }
-  
-console.log(confirmEnding("Bastian", "n"));
+// console.log(confirmEnding("Bastian", "n"));
 console.log(confirmEnding("Congratulation", "on"));

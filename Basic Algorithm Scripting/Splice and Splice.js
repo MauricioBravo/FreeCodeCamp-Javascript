9fd208d2e82171676a2418e6d3b27df75fc5c721
MauricioBravo <qwe455r@gmail.com>
2023-09-04 19:06:38 -0300

    Splice and Splice started

---
+++ Basic Algorithm Scripting/Splice and Splice.js
@@ -0,0 +1,18 @@
+function frankenSplice(arr1, arr2, n) {
+    var arr =arr1;
+
+    // 1 dividir el segundo array
+    // 2 hace que la frecuencia de repeticion de el push sea el valor que recibe la funcion,
+    // para que por ejemplo si el numero es 2 se metan 2 elementos adelante y dos atrás del array principal.
+    for(let i=0; i<n;i++){
+ 
+        // separar el array 2 desde la posicion n
+        //le agrego todo el arr1 con el for hasta .leght desde la posicion n que es donde se parte
+    
+    }
+
+    return arr2;
+    
+  }
+  console.log(frankenSplice([1, 2, 3], [4, 5], 1));
+  console.log(frankenSplice([1, 2, 3,4], [5,6,7,8], 1));

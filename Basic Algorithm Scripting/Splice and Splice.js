e54f717a4e7192c20fb6ca90657d56eb95b04dbc
MauricioBravo <qwe455r@gmail.com>
2023-09-12 18:40:51 -0300

    Splice and Splice finished

--- Basic Algorithm Scripting/Splice and Splice.js
+++ Basic Algorithm Scripting/Splice and Splice.js
@@ -1,38 +1,37 @@
 function frankenSplice(arr1, arr2, n) {
-    var arr =arr2;
-console.log(arr.length);
-    // 1 dividir el segundo array
-    // 2 hace que la frecuencia de repeticion de el push sea el valor que recibe la funcion,
-    // para que por ejemplo si el numero es 2 se metan 2 elementos adelante y dos atrás del array principal.
-    
-    
-    
-    
-    for(let i=0; i<arr1.length;i++){
-        console.log(arr);
-        console.log(i);
-        
-        // lo que pasa es que cuando el array tiene 4 elementos o más su mitad para ubicar el nuevo elemento es una posición incorrecta
-        
-        if(arr.length%2 == 0){
+    const arrBackup = [].concat(arr2);
 
-            arr.splice(arr.length/2,null,arr1[i]);
-            console.log(arr1[i]);
-        }
-        else{
-            arr.splice(arr.length/2+1,null,arr1[i]);
-        }
-         // corregir esta linea que funciona al principio y luego no*************
-       
-        
-    //Array.splice(start, removeCount, newItem, newItem, newItem, ...)
+    //asigno a arr2 y a backup el mismo array original, la pregunta es, por que cambia si nunca asigno un nuevo valor a backup?
+    console.log(arr2);
+    console.log(arrBackup);
 
+    var arr =  [].concat(arr2);
+    var arrDividido2 = arr.length / 2;
 
+    if (n == 0) {
+        return arr1;
     }
+    else {
+        for (let i = 0; i < arr1.length; i++) {
 
+            if (arr.length % 2 == 0) {
+                arr.splice(arrDividido2 + i, null, arr1[i]);
+                console.log(arr2);
+            }
+            else {
+                arr.splice(arrDividido2 + i, null, arr1[i]);
+            }
+            //Array.splice(start, removeCount, newItem, newItem, newItem, ...)
+
+        }
+    }
+    //******* valor de backup cambiado sin ninguna explicación aparente  */
+    console.log(arrBackup);
+    arr2 = arrBackup;
+    console.log(arr2);
     return arr;
-    
-  }
-  console.log(frankenSplice([1, 2], ["a", "b"], 1));//should return ["a", 1, 2, "b"])
-  console.log(frankenSplice([1, 2, 3], [4, 5], 1));
+
+}
+console.log(frankenSplice([1, 2], ["a", "b"], 1));//should return ["a", 1, 2, "b"])
+console.log(frankenSplice([1, 2, 3], [4, 5], 1));
 //   console.log(frankenSplice([1, 2, 3,4], [5,6,7,8], 1));

5aebc56d90d2c18832a7c61ccb6bd8942d598f03
MauricioBravo <qwe455r@gmail.com>
2023-09-18 10:25:50 -0300

    mutations finished

--- Basic Algorithm Scripting/mutations.js
+++ Basic Algorithm Scripting/mutations.js
@@ -1,49 +1,46 @@
 function mutation(arr) {
-    var flag=false;
-    var arr1="";
-    var arr2=[];
-    arr1=arr[0].toString();
+    var flag = false;
+    var arr1 = "";
+    var arr2 = [];
+    arr1 = arr[0].toString();
     console.log(arr1);
-    arr1=arr1.toLowerCase();
+    arr1 = arr1.toLowerCase();
     console.log(arr1);
 
-
-    arr2=arr[1].toString();
+    arr2 = arr[1].toString();
     console.log(arr2);
-    arr2=arr2.toLowerCase();
+    arr2 = arr2.toLowerCase();
     console.log(arr2);
 
-// el programa no debe tener orden porque debe ser capaz de detectar 
-//  las letras independientes de su posicion
-
-
-    for(var i=0; i < arr2.length; i++){
-        console.log(arr1[i]);
-        for(var j=0;j<arr1.length;j++){ 
-            console.log(arr2[j]);
-            if(arr1[i]==arr2[j] && arr2){
+    // el programa no debe tener orden porque debe ser capaz de detectar 
+    //  las letras independientes de su posicion
+    for (var i = 0; i < arr2.length; i++) {
+        console.log(arr2[i]);
+        for (var j = 0; j < arr1.length; j++) {
+            console.log(arr1[j]);
+            if (arr2[i] == arr1[j]) {
                 console.log(arr2[j]);
-                flag=true;
-                j=arr1.length;
+                flag = true;
+                j = arr1.length;
             }
-            
-            if(j!=arr1.length){
-                flag=false;
+
+            if (j != arr1.length) {
+                flag = false;
             }
-            
         }
-      
-        
+        if(flag==false){
+            i = arr2.length;
+        }
+
 
     }
-
     console.log(flag);
-    
     return flag;
-  }
-console.log(mutation(["hello", "Hello"]));
-console.log(mutation(["hello", "hey"]));
-console.log(mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]));// debe ser true
+}
+console.log( mutation(["hello", "neo"]) );
+// console.log(mutation(["hello", "Hello"]));
+// console.log(mutation(["hello", "hey"]));
+// console.log(mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]));// debe ser true
 
 // poner la comparacion en orden para revisar la letra del arra2 en el arr1 y en caso de ser la ultima
 // del arr2 si no encuentra la letra devolver falso
